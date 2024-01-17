78c7159829c29c85926884f20190109ad4f47b1a
Иван Былёв <ivan.bylyov@gmail.com>
2024-01-17 13:41:21 +0300

    Initial commit

---
+++ app/src/main/java/com/vortex/android/vortexdictionary/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.vortex.android.vortexdictionary
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
