c24d018a3f422cd45750aa32f8025735703ce686
Иван Былёв <ivan.bylyov@gmail.com>
2024-01-18 14:10:48 +0300

    Добавлен Navigation Drawer с черновым дизайном

--- app/src/main/java/com/vortex/android/vortexdictionary/MainActivity.kt
+++ app/src/main/java/com/vortex/android/vortexdictionary/MainActivity.kt
@@ -2,11 +2,29 @@ package com.vortex.android.vortexdictionary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.navigation.NavigationView
 import com.vortex.android.vortexdictionary.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private var _appBarConfiguration: AppBarConfiguration? = null
+    private val appBarConfiguration
+        get() = checkNotNull(_appBarConfiguration)
+
+    private var _navController: NavController? = null
+    private val navController
+        get() = checkNotNull(_navController)
 
     private var _binding: ActivityMainBinding? = null
     private val binding
@@ -19,10 +37,38 @@ class MainActivity : AppCompatActivity() {
 
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setupNavigationDrawer()
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+        _navController = null
+        _appBarConfiguration = null
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item, navController)
+                || super.onOptionsItemSelected(item)
+    }
+
+    private fun setupNavigationDrawer() {
+        setSupportActionBar(binding.appBarMain.toolbar)
+        val drawerLayout: DrawerLayout = binding.drawerLayout
+        val navView: NavigationView = binding.navView
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        _navController = navHostFragment.navController
+        _appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.wordCardFragment, R.id.dictionary_item, R.id.settings_item
+            ), drawerLayout
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
     }
 }
